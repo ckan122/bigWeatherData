97e5e04ed5f42a8499ad722f33d3a173c9ba5726
Dogeon188 <dogcatdog188@gmail.com>
2021-04-02 16:43:45 +0800

    dist

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
+import sys
 import time
-import json
 from os import environ
-from pathlib import Path
 
 import eel
 import requests
 from dotenv import load_dotenv
 
-from lib.myquery import MyQuery
-
 load_dotenv()
-
-databaseHost = "https://opendata.cwb.gov.tw/"
-retrievingDataId = "/v1/rest/datastore/F-C0032-001"
-dataSrc = MyQuery(databaseHost + "api" + retrievingDataId) \
-    .query("Authorization", environ.get("AUTHKEY")) \
-    .query("sort", "time")
-apiDocSrc = databaseHost + "apidoc/v1"
-
-eel.init("web")
-
+# load_dotenv(dotenv_path=sys._MEIPASS + "/.env")
+dataSrc = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?sort=time&Authorization=" \
+          + environ.get('AUTHKEY')
 weather_data = None
+eel.init("web")
 
 
 def parse_time(timestamp: str):
-    # date = time.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
     return time.strftime("%m/%d %H時", time.strptime(timestamp, '%Y-%m-%d %H:%M:%S'))
 
 
@@ -33,14 +23,11 @@ def fetch_weather_data():
     print("Fetching weather data...")
     try:
         raw_data = requests.get(dataSrc).json()
-        Path("./weather.json").write_text(json.dumps(raw_data, indent=2, ensure_ascii=False), encoding="utf-8")
         if raw_data["success"] != "true":
             raise requests.exceptions.RequestException(
                 "Something went wrong on the database server when fetching weather weather_data!")
     except requests.exceptions.RequestException:
         print("Failed on fetching weather data!")
-        print("Loading from cached weather_data instead...")
-        raw_data = json.loads(Path("./weather.json").read_text(encoding="utf-8"))
 
     weather_data = dict()
 
@@ -65,4 +52,5 @@ def get_weather_data():
 
 
 if __name__ == "__main__":
-    eel.start("index.html", mode="chrome-app")
+    fetch_weather_data()
+    eel.start("index.html")
--- web/script/main.js
+++ web/script/main.js
@@ -1,4 +1,8 @@
 let weather_data;
+const factor = 0.8;
+
+let img = $("#city-map-img");
+img.width(img.width() * factor).height(img.height() * factor)
 
 eel.get_weather_data()(raw_data => {
     weather_data = raw_data;
@@ -8,19 +12,14 @@ eel.get_weather_data()(raw_data => {
         city.append(info);
         Object.values(cityData).forEach(wdata => {
             info.append(`<div class="weather-info">
-            <div class="weather-info-time">${wdata.startT}</div>
-            <div class="weather-info-time">${wdata.endT}</div>
-            <img src="assets/weather/${wximg[wdata.WxImg]}.svg" alt="${wdata.Wx}" width="50" height="50"/>
-            <div class="weather-info-wc">
-                <div class="weather-info-wx">${wdata.Wx}</div>
-                <div class="weather-info-ci">${wdata.CI}</div>
-            </div>
-            <p class="weather-info-t">
+            <div>${wdata.startT}</div><div>${wdata.endT}</div>
+            <img src="assets/weather/${wximg[wdata.WxImg]}.svg" alt="${wdata.Wx}2" width="50" height="50"/>
+            <div>${wdata.Wx}</div><div>${wdata.CI}</div>
+            <p>
             <img src="assets/weather/1f321.svg" alt="氣溫：" height="24" width="24"/>
-            <span style="color: skyblue">${wdata.MinT}</span> ~ 
-            <span style="color: firebrick">${wdata.MaxT}</span>⁰C
+            <span style="color: skyblue">${wdata.MinT}</span> ~ <span style="color: firebrick">${wdata.MaxT}</span>⁰C
             </p>
-            <p class="weather-info-pp">
+            <p>
             <img src="assets/weather/${(30 < parseInt(wdata.PoP)) ? "2614" : "2602"}.svg" alt="降雨機率：" height="24" width="24">${wdata.PoP}%
             </p>
             </div>`);
@@ -29,24 +28,15 @@ eel.get_weather_data()(raw_data => {
     }
 
     for (let city of cities) {
-        let node = $(`<area shape="poly" coords="${city.coords}" alt="${city.name}" class="city-box" title="${city.name}">`)
+        let node = $(`<area shape="poly" coords="${city.coords.map(x => x * factor)}" alt="${city.name}" class="city-box" title="${city.name}">`)
         $("#city-map").append(node);
         node.on("mouseenter", () => {
             $(`.weather-city`).hide()
-            // $(`.weather-city:visible:not([data-city-name=${city.name}])`).hide()
             $(`.weather-city[data-city-name=${city.name}]`).show()
             let pos = $("#city-select").position();
             let cur = $("#city-map-cursor");
             if (city.pos === undefined) city.pos = [0, 0];
-            cur.css({left : pos.left + city.pos[0], top: pos.top + city.pos[1]})
+            cur.css({left : pos.left + city.pos[0] * factor, top: pos.top + city.pos[1] * factor})
         });
     }
 });
-
-/*
-$(document).on("mousemove", function getCursorXY(e) {
-    let pos = $("#city-select").position();
-    $("#cursorX").text(Math.round(e.pageX - pos.left).toString());
-    $("#cursorY").text(Math.round(e.pageY - pos.top).toString());
-    $("#foo").css({left: e.pageX, top: e.pageY})
-})*/
